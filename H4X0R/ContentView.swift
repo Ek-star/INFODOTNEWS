a12467df89bd6c32bcda6e8ab9f79c5a7d863603
Ek-star <totoekalma@gmail.com>
2025-06-04 19:21:46 +0700

    Initial Commit

---
+++ H4X0R/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  H4X0R
+//
+//  Created by EKALMA TOTO ALLOY SEMBIRING on 04/06/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
